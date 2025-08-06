5ec131e53e70410c25cd83d893ecb5b7a3f1be67
Rushikesh tanaji shinde <shinderushikesh7108@gmail.com>
2025-08-06 15:22:07 +0530

    updated

--- src/TNS/SampleExample.java
+++ src/TNS/SampleExample.java
@@ -5,6 +5,7 @@ public class SampleExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("It Worked!");
+		System.out.println("It Worked!");
 
 	}
 
