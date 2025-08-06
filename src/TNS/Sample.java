54d62c7942852897739fb095de346d7f885eac79
Rushikesh tanaji shinde <shinderushikesh7108@gmail.com>
2025-08-06 15:10:46 +0530

    Commited Sample.java file

---
+++ src/TNS/Sample.java
@@ -0,0 +1,11 @@
+package TNS;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello, This is Rushikesh.");
+	}
+
+}
